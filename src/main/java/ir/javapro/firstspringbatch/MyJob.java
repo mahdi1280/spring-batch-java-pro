d4a389e928e0399b50adfb1c053746a8bea761c9
mahdikarimi422@gmail.com <mahdikarimi422@gmail.com>
2026-07-05 14:01:18 +0330

    10-04: partitioner implement

--- src/main/java/ir/javapro/firstspringbatch/MyJob.java
+++ src/main/java/ir/javapro/firstspringbatch/MyJob.java
@@ -17,11 +17,13 @@ import org.springframework.batch.core.job.parameters.JobParametersValidator;
 import org.springframework.batch.core.listener.ItemReadListener;
 import org.springframework.batch.core.listener.JobExecutionListener;
 import org.springframework.batch.core.listener.StepExecutionListener;
+import org.springframework.batch.core.partition.Partitioner;
 import org.springframework.batch.core.repository.JobRepository;
 import org.springframework.batch.core.step.Step;
 import org.springframework.batch.core.step.StepExecution;
 import org.springframework.batch.core.step.builder.StepBuilder;
 import org.springframework.batch.infrastructure.item.Chunk;
+import org.springframework.batch.infrastructure.item.ExecutionContext;
 import org.springframework.batch.infrastructure.item.ItemProcessor;
 import org.springframework.batch.infrastructure.item.ItemWriter;
 import org.springframework.batch.infrastructure.item.database.JpaItemWriter;
@@ -35,28 +37,60 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.core.task.AsyncTaskExecutor;
 import org.springframework.core.task.SimpleAsyncTaskExecutor;
 import org.springframework.core.task.TaskExecutor;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.PlatformTransactionManager;
 
+import javax.sql.DataSource;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Objects;
 
 @Configuration
 public class MyJob {
 
     private final PersonService personService;
+    private final DataSource dataSource;
 
-    public MyJob(PersonService personService) {
+    public MyJob(PersonService personService, DataSource dataSource) {
         this.personService = personService;
+        this.dataSource = dataSource;
     }
 
     @Bean
-    public JpaPagingItemReader<Person> jpaPersonReader(EntityManagerFactory entityManagerFactory) {
+    public Partitioner partitioner() {
+        return gridSize -> {
+            JdbcTemplate jdbcTemplate = new JdbcTemplate(dataSource);
+            long min =  jdbcTemplate.queryForObject("select min(id) from person", Long.class);
+            long max =  jdbcTemplate.queryForObject("select max(id) from person", Long.class);
+            Map<String, ExecutionContext> result = new HashMap<>();
+            long targetSize = (max - min) / gridSize + 1;
+            long start = min;
+            long end = start + targetSize + 1;
+            for (int i = 0; i < gridSize; i++) {
+                ExecutionContext context = new ExecutionContext();
+                context.putLong("min", start);
+                context.putLong("max", Math.min(end, max));
+                result.put("p"+i, context);
+                start +=targetSize;
+                end+=targetSize;
+            }
+            return result;
+        };
+    }
+
+    @Bean
+    @StepScope
+    public JpaPagingItemReader<Person> jpaPersonReader(EntityManagerFactory entityManagerFactory,
+                                                       @Value("#{stepExecutionContext['min']}") Long min,
+                                                       @Value("#{stepExecutionContext['max']}") Long max) {
         return new JpaPagingItemReaderBuilder<Person>()
                 .name("personReader")
                 .entityManagerFactory(entityManagerFactory)
-                .queryString("SELECT p FROM Person p order by p.id")
+                .queryString("SELECT p FROM Person p where p.id between :min and :max order by p.id")
+                .parameterValues(Map.of("min", min, "max", max))
                 .pageSize(100)
                 .build();
     }
@@ -89,14 +123,23 @@ public class MyJob {
                      JpaPagingItemReader<Person> jpaPersonReader,
                      SynchronizedItemStreamReader<Person> synchronizedItemStreamReader,
                      ItemWriter<Person> jpaPersonWriter, AsyncTaskExecutor taskExecutor) {
-
         return new StepBuilder("step1", jobRepository)
                 .<Person, Person>chunk(100)
-                .reader(synchronizedItemStreamReader(jpaPersonReader))
+                .reader(jpaPersonReader)
                 .processor(processor)
                 .writer(jpaPersonWriter)
-                .taskExecutor(taskExecutor)
+//                .taskExecutor(taskExecutor)
                 .transactionManager(transactionManager)
+                .build();
+    }
+
+    @Bean
+    public Step managerStep(JobRepository jobRepository, Partitioner partitioner, Step step , AsyncTaskExecutor taskExecutor ) {
+        return new StepBuilder("managerStep", jobRepository)
+                .partitioner("worker", partitioner)
+                .step(step)
+                .gridSize(8)
+                .taskExecutor(taskExecutor)
                 .build();
     }
 
@@ -112,9 +155,9 @@ public class MyJob {
     }
 
     @Bean
-    public Job job(JobRepository jobRepository, Step step) {
+    public Job job(JobRepository jobRepository, Step managerStep) {
         return new JobBuilder("job" ,jobRepository)
-                .start(step)
+                .start(managerStep)
                 .build();
     }
 }
